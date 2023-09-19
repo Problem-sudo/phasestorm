27217f980573f1549e07cb31fea3c9e6364f3a75
problem-sudo <anitmaurya563@gmail.com>
2023-09-20 01:43:05 +0530

    Changed API key

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "2648f59fe77902473cde29b4b9826fb5",
+  key: "b9f65dc206806bba141ebb8c444ccee3",
   base: "https://api.openweathermap.org/data/2.5/",
 };
