4dae1c14de46840bd3b4e4c375810a0c1225e5b0
problem-sudo <anitmaurya563@gmail.com>
2023-09-20 01:33:11 +0530

    Changed build cmd

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "7dd87093e1683c9669f44286306d3d28",
+  key: "2648f59fe77902473cde29b4b9826fb5",
   base: "https://api.openweathermap.org/data/2.5/",
 };
